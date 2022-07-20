46f60595475ced141b60328f45ef628604fc6585
Gautier Fondevila <fondevila.gautier@gmail.com>
2022-07-20 19:20:35 +0200

    create fixtures, .twig, controller, and scss. All that is done (for the index)

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PortfolioRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class PortfolioController extends AbstractController
 {
     #[Route('/portfolio', name: 'app_portfolio')]
-    public function index(): Response
+    public function index(PortfolioRepository $portfolioRepository): Response
     {
         return $this->render('portfolio/index.html.twig', [
-            'controller_name' => 'PortfolioController',
+            'projects' => $portfolioRepository->findAll()
         ]);
     }
 }
