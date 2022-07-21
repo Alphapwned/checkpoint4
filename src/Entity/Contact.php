722459e20661007832e63a05c791b2b68f1bc5bc
Gautier Fondevila <fondevila.gautier@gmail.com>
2022-07-21 09:46:54 +0200

    contact entity created, contact fixtures created, and migrations ok.

---
+++ src/Entity/Contact.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Entity;
+
+use DateTimeInterface;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ContactRepository;
+use Symfony\Component\Validator\Constraints as Assert;
+use DateTime;
+
+#[ORM\Entity(repositoryClass: ContactRepository::class)]
+class Contact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Please fill in this field')]
+    #[Assert\Length(max: 255, maxMessage: 'The firstname entered is too long, it must not exceed {{ limit }} characters')]
+    #[Assert\Type('string')]
+    private string $firstname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Please fill in this field')]
+    #[Assert\Length(max: 255, maxMessage: 'The lastname entered is too long, it must not exceed {{ limit }} characters')]
+    #[Assert\Type('string')]
+    private string $lastname;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: 'Please fill in this field')]
+    #[Assert\Length(max: 255, maxMessage: 'The email entered is too long, it must not exceed {{ limit }} characters')]
+    #[Assert\Email(message: 'The email address entered is not valid')]
+    private string $email;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    #[Assert\Type('string')]
+    private ?string $message = null;
+
+    #[ORM\Column(type: 'datetime')]
+    private DateTimeInterface $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstname(): ?string
+    {
+        return $this->firstname;
+    }
+
+    public function setFirstname(string $firstname): self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    public function getLastname(): ?string
+    {
+        return $this->lastname;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(?string $message = ''): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(): self
+    {
+        $this->createdAt = new Datetime('now');
+
+        return $this;
+    }
+}
