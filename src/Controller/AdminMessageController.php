056c32a5e3170a10763864f7f81d714d672fccfc
Gautier Fondevila <fondevila.gautier@gmail.com>
2022-07-21 20:08:28 +0200

    remove some useless things

--- src/Controller/AdminMessageController.php
+++ src/Controller/AdminMessageController.php
@@ -3,11 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Contact;
-use App\Form\Contact1Type;
 use App\Repository\ContactRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
